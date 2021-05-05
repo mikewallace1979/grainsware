6e6ff155e6f095418e2d36a162616250982dd766
Michael Wallace <mikewallace1979@googlemail.com>
2021-05-05 20:36:34 +0100

    Stabilize the clockulator (#7)
    Simplify the clockulator by getting rid of multiplication for now because
    it was poorly implemented and resulted in the clock signal being out of
    time and therefore basically useless.

--- clockulator.ino
+++ clockulator.ino
@@ -1,6 +1,6 @@
 /*
 
-A probabilistic clock divider/multiplier for the Ginko Synthese Grains eurorack module.
+A probabilistic clock divider for the Ginko Synthese Grains eurorack module.
 
 Derived from code by a773 (atte.dk):
 - https://github.com/attejensen/a773_grains/blob/784c28658dfbdefe98246051a63527a0c8f1063b/mult_div/mult_div.ino
@@ -13,14 +13,14 @@ User guide:
 
 # Input 1 / Upper pot
 
-- Probability that a division will actually trigger.
-- Fully counter-clockwise nothing will be sent to the output.
-- Fully clockwise every division will be sent to the output.
+- Size of the resulting divisions relative to the incoming pulse.
+- From fully CCW to fully CW: 16, 8, 4, 2, 1.
 
 # Input 2 / Middle pot
 
-- Size of the resulting divisions relative to the incoming pulse.
-- From fully CCW to fully CW: 4, 2, 1, 0.25.
+- Probability that a division will actually trigger.
+- Fully counter-clockwise nothing will be sent to the output.
+- Fully clockwise every division will be sent to the output.
 
 # Input 3
 
@@ -48,9 +48,6 @@ buffer it through something else it's fine. I don't know what's going
 on there but I'm guessing it's related to the output being low-pass
 filtered?
 
-The other gotcha is that the clock div/mult logic is really basic
-and only cares about the next interval.
-
 Also the pots are not linear but the mapping to values is linear so
 bear that in mind (or file a PR... :P).
 
@@ -76,6 +73,14 @@ boolean in_clock_high = false;
 long last_trigger_in = 0;
 long next_trigger_out = 0;
 long last_trigger_out = 0;
+byte last_clock_factor = 1;
+byte skip = last_clock_factor;
+
+byte clock_factor = 1;
+int gate = 0;
+long current_time = millis ();
+int trigger_length = TRIGGER_LENGTH;
+
 
 int gate_length = TRIGGER_LENGTH;
 int interval = 0;
@@ -119,16 +124,18 @@ mode get_mode() {
   }
 }
 
-float get_clock_factor() {
-  switch(map(analogRead(UPPER_POT), 0, UPPER_POT_MAX, 0, 3)) {
+byte get_clock_factor() {
+  switch(map(analogRead(UPPER_POT), 0, UPPER_POT_MAX, 0, 4)) {
     case 0:
-      return 4;
+      return 16;
     case 1:
-      return 2;
+      return 8;
     case 2:
-      return 1;
+      return 4;
     case 3:
-      return 0.5;
+      return 2;
+    case 4:
+      return 1;
   }
 }
 
@@ -143,11 +150,10 @@ boolean should_trigger() {
 void loop() 
 {
   mode current_mode = get_mode();
-  float clock_factor = get_clock_factor();
-  int gate = analogRead(CLOCK_IN);
-  long current_time = get_time();
-  boolean clock_identity = clock_factor > 0.5 && clock_factor < 2;
-  int trigger_length = current_mode == mode_gate ? gate_length : TRIGGER_LENGTH;
+  clock_factor = get_clock_factor();
+  gate = analogRead(CLOCK_IN);
+  current_time = get_time();
+  trigger_length = current_mode == mode_gate ? gate_length : TRIGGER_LENGTH;
 
   // Determine whether any outgoing gate/trigger needs to be zero'd
   if (out_high && current_mode != mode_random_cv && (current_time - last_trigger_out) > trigger_length) {
@@ -155,33 +161,20 @@ void loop()
     analogWrite(PWM_PIN, 0);
   }
 
-  // Determine if we should trigger and do so
-  if (!clock_identity && interval > 0 && next_trigger_out < current_time) {
-    trigger(current_mode);
-    if (last_trigger_out > 0) {
-      gate_length = (current_time - last_trigger_out) / 2;
-    }
-    last_trigger_out = current_time;
-    next_trigger_out = current_time + (int) (interval * clock_factor);
-  }
-
   // Update the state based on incoming clock
   if (gate > GATE_THRESHOLD) {
     if (!in_clock_high) {
       // Rising edge
-      if (clock_identity) {
-        // If the factor is 1 then short circuit the clock tracking and just trigger
+      skip -= 1;
+      if (skip == 0 || last_clock_factor != clock_factor) {
+        skip = clock_factor;
+        // Maybe trigger
         trigger(current_mode);
         if (last_trigger_out > 0) {
           gate_length = (current_time - last_trigger_out) / 2;
         }
         last_trigger_out = current_time;
       }
-      // Update the interval
-      if (last_trigger_in > 0) {
-        interval = current_time - last_trigger_in;
-      }
-      last_trigger_in = current_time;
       in_clock_high = true;
     }
   } else {
@@ -190,4 +183,5 @@ void loop()
       in_clock_high = false;
     }
   }
+  last_clock_factor = clock_factor;
 }
